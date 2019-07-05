d28c8ad55807fadca27471ff9263709df20dc186
Julie Gankina <qunya11@gmail.com>
2019-07-05 23:12:52 +0300

    JS buttons updated

--- Technomart/js/store-script.js
+++ Technomart/js/store-script.js
@@ -1,14 +1,19 @@
-
-var linkItemAdded = document.querySelector(".tocart");
+var linkItemAdded = document.querySelectorAll(".tocart");
 var popupItemAdded = document.querySelector(".item-added");
 var closeItemAdded = popupItemAdded.querySelector(".modal-close");
 var closeButton = popupItemAdded.querySelector(".button-close");
-
-/*feedback modal block*/
-linkItemAdded.addEventListener("click", function (evt) {
+var popupOpen = function(link) {
+  link.addEventListener("click", function (evt) {
   evt.preventDefault();
+
   popupItemAdded.classList.add("modal-show");
 });
+}
+
+for (var i=0; i<linkItemAdded.length; i++) {
+  console.log('opening popup');
+  popupOpen(linkItemAdded[i]);
+}
 
 closeItemAdded.addEventListener("click", function (evt) { evt.preventDefault();
 popupItemAdded.classList.remove("modal-show");
